b4aa7468ecd2cbe6c0691b55d92e6f53022c6272
Caboose-Chengkun-Liao <jamessmithhunk@163.com>
2026-07-23 12:45:11 -0400

    Added axios interceptors for 401 response

--- src/lib/request.ts
+++ src/lib/request.ts
@@ -2,7 +2,20 @@ import axios from "axios";
 
 const request = axios.create({
     baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
-    timeout: 10000
+    timeout: 10000,
+    withCredentials: true
 });
+
+request.interceptors.response.use(
+    (res) => res,
+    (error) => {
+        if(error.response?.status === 401) {
+            //Avoid refresh when login failed; avoid triggered in cli"
+            if(typeof window !== "undefined" && window.location.pathname !== "/login")
+                window.location.href = "/login";
+        }
+        return Promise.reject(error);
+    }
+)
 
 export default request;
