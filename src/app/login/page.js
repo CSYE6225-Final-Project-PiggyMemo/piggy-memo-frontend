ee37a02bf9d797cf84c142be5ea381f666e6dbe4
Caboose-Chengkun-Liao <jamessmithhunk@163.com>
2026-07-23 17:44:05 -0400

    Fixed a minor error

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,5 +1,6 @@
+"use client";
 import { login } from "@/api/auth"
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 
 export default function Home() {
